2b493cbe3d4940b35c7aa7be3cb52ef28bd14552
Dxvah <idayraperezdelgado@gmail.com>
2022-11-25 12:31:14 +0000

    interfaz creada

---
+++ farm/Assets/Script/Camera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    Vector3 mousepos = Input.mousePosition;
+
+    
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            
+            //getintothemouseposition con input
+
+        }
+    }
+}
